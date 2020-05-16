fb00b75a98a95f7e8f0834ce69a9e5823d761a7f
ecker <textmodelview@gmail.com>
2020-05-16 22:12:32 +0200

    carefully prettified again

--- texeltree.nim
+++ texeltree.nim
@@ -8,28 +8,28 @@ const
   NMIN = int(NMAX / 2)
 
 type Texel* = ref object of RootObj
-  
+
 type Single* = ref object of Texel
   text*: string # XXX Sollte immutable sein!
   style*: Style
-  
+
 type Text* = ref object of Texel
   text*: string # XXX Sollte immutable sein!
   style*: Style
-  
+
 type TexelWithChilds = ref object of Texel
   childs: seq[Texel]
   length: int
   nlines: int
-  
+
 type Container* = ref object of TexelWithChilds
-  
+
 type Group* = ref object of TexelWithChilds
   depth: int
-  
+
 type NewLine* = ref object of Single
   parstyle: Style
-  
+
 type Tabulator* = ref object of Single
 
 
@@ -41,18 +41,18 @@ proc get_childs*(texel: Texel): seq[Texel] =
 method get_depth*(this: Texel): int {.base.} = 0
 method get_depth*(this: Group): int = this.depth
 
-  
+
 method get_length*(this: Texel): int {.base.} = 0
 method get_length*(this: Single): int = 1
 method get_length*(this: Text): int = this.text.len
 method get_length*(this: TexelWithChilds): int = this.length
 
-  
+
 method get_nlines*(this: Texel): int {.base.} = 0
 method get_nlines*(this: NewLine): int = 1
 method get_nlines*(this: Group): int = this.nlines
 
-  
+
 method `$`*(this: Texel): string {.base.} = "Texel()"
 method `$`*(this: Single): string = "S[" & escape(this.text) & "]"
 method `$`*(this: Text): string = "T[" & escape(this.text) & "]"
@@ -64,14 +64,14 @@ method `$`*(this: Group): string =
 method `$`*(this: NewLine): string = "NL"
 method `$`*(this: Tabulator): string = "TAB"
 
-  
+
 proc copy*(this: Single, style: Option[Style]): Single =
   var new: Single
   deepCopy(new, this)
   if not style.isNone:
     new.style = style.get()
   return new
-  
+
 proc copy*(this: Text, style: Option[Style],
            text: Option[string]): Text =
   var new: Text
@@ -82,21 +82,21 @@ proc copy*(this: Text, style: Option[Style],
     new.text = text.get()
   return new
 
-  
+
 proc sum_length(l: seq[Texel]): int =
   var r = 0
   for texel in l:
     r += texel.get_length()
   return r
 
-  
+
 proc sum_nlines(l: seq[Texel]): int =
   var r = 0
   for texel in l:
     r += texel.get_nlines()
   return r
 
-  
+
 proc init*(texel: Container) =
   texel.length = sum_length(texel.childs)
   texel.nlines = sum_nlines(texel.childs)
@@ -141,7 +141,7 @@ iterator iter_childs*(texel: Texel): tuple[i1: int, i2: int, child: Texel] =
     i2 = i1+n
     yield (i1, i2, child)
 
-    
+
 let SPACE* = Single(text: " ", style: EMPTYSTYLE)
 let NL* = NewLine(text: "\n", style: EMPTYSTYLE)
 let TAB* = Tabulator(text: "\t", style: EMPTYSTYLE)
@@ -219,21 +219,21 @@ proc join*(l1, l2, l3: seq[Texel]): seq[Texel] =
 proc join*(l1, l2, l3, l4: seq[Texel]): seq[Texel] =
   return join(join(join(l1, l2), l3), l4)
 
-  
+
 proc get_rightmost(texel: Texel): Texel =
-  assert length(texel)>0
+  assert length(texel) > 0
   if texel of TexelWithChilds:
     return get_rightmost(get_childs(texel)[^1])
   return texel
 
-  
+
 proc get_leftmost(texel: Texel): Texel =
-  assert length(texel)>0
+  assert length(texel) > 0
   if texel of TexelWithChilds:
     return get_leftmost(get_childs(texel)[0])
   return texel
 
-  
+
 proc exchange_rightmost(texel, new: Texel): Texel =
   if texel of TexelWithChilds:
     let cl = texel.get_childs()
@@ -241,7 +241,7 @@ proc exchange_rightmost(texel, new: Texel): Texel =
     return Group(childs: cl[0..^2] & @[l])
   return new
 
-  
+
 proc without_leftmost(texel: Texel): seq[Texel] =
   let childs = get_childs(texel)
   if len(childs) == 0:
@@ -249,20 +249,20 @@ proc without_leftmost(texel: Texel): seq[Texel] =
   let l = without_leftmost(childs[0])
   return join(l, childs[1..^1])
 
-  
+
 proc can_merge(texel1, texel2: Texel): bool =
   if texel1 of Text and texel2 of Text and
       Text(texel1).style == Text(texel2).style:
     return true
   return false
 
-  
+
 proc merge(texel1, texel2: Texel): seq[Texel] =
   let t1 = Text(texel1)
   let t2 = Text(texel2)
   return @[Texel(Text(text: t1.text & t2.text, style: t1.style))]
 
-  
+
 proc fuse*(l1, l2: seq[Texel]): seq[Texel] =
   ## Like join(...) but also merge the arguments if possible.
   ## The returned list is homogeneous.
@@ -276,36 +276,36 @@ proc fuse*(l1, l2: seq[Texel]): seq[Texel] =
     return join(l1, l2)
   return join(l1[0..^2], merge(t1, t2), without_leftmost(l2[0]),
               l2[1..^1])
-              
+
 proc fuse*(l1, l2, l3: seq[Texel]): seq[Texel] =
   ## Like join(...) but also merge the arguments if possible.
   ## The returned list is homogeneous.
   return fuse(fuse(l1, l2), l3)
-    
-    
+
+
 proc strip(root: Texel): Texel =
   ## Removes unnecessary Group-elements from the root.
   var root: Texel = root
   while root of Group and len(Group(root).childs) == 1:
     root = Group(root).childs[0]
   return root
-                
-                
+
+
 proc grouped(stuff: seq[Texel]): Texel =
   ## Creates a single group from the list of texels *stuff*.
   var l: seq[Texel] = stuff
   while len(l) > NMAX:
     l = groups(l)
   return strip(newGroup(l))
-    
-    
+
+
 proc insert*(texel: Texel, i: int, stuff: seq[Texel]): seq[Texel] =
   ## Inserts the seq `stuff` at position `i`.
   ##
   ## `Texel` must be root-efficient, `stuff` must be
   ## list-efficient. Note that insert can increase the texels
   ## depth. The returned list is always list efficient.
-  
+
   if not (0 <= i and i <= length(texel)):
     raise newException(IndexError, "index out of bounds: " & repr(i))
 
@@ -357,10 +357,10 @@ proc insert*(texel: Texel, i: int, stuff: seq[Texel]): seq[Texel] =
         n[k] = grouped(insert(child, i-i1, stuff))
         return @[Texel(container.copy(childs = option(n)))]
   assert 1 == 0
-          
-          
+
+
 proc get_text*(texel: Texel): string =
-  ## Get the content of `texel`as a text string which has the same length.  
+  ## Get the content of `texel`as a text string which has the same length.
   if texel of Text:
     return Text(texel).text
   if texel of Single:
@@ -372,8 +372,8 @@ proc get_text*(texel: Texel): string =
     return r
   echo "Texel=", repr(texel)
   assert false
-        
-        
+
+
 when isMainModule:
   import unittest
   suite "testing texeltree.nim":
@@ -410,44 +410,44 @@ when isMainModule:
       #echo $h
 
     test "insert()":
-        var i: int = 1
-        assert 0 <= i
-        var b = not (0 <= i and i <= 2)
-        assert b == false
-        check(get_text(g) == "\n\t")
+      var i: int = 1
+      assert 0 <= i
+      var b = not (0 <= i and i <= 2)
+      assert b == false
+      check(get_text(g) == "\n\t")
 
-        var t = Text(text: "Chris")
-        check(get_text(grouped(g.insert(0, @[Texel(t)]))) == "Chris\n\t")
-        check(get_text(grouped(g.insert(1, @[Texel(t)]))) == "\nChris\t")
-        check(get_text(grouped(g.insert(2, @[Texel(t)]))) == "\n\tChris")
-        expect(IndexError):
-          discard g.insert(-1, @[Texel(t)])
-      
-        expect(IndexError):
-          discard g.insert(3, @[Texel(t)])
+      var t = Text(text: "Chris")
+      check(get_text(grouped(g.insert(0, @[Texel(t)]))) == "Chris\n\t")
+      check(get_text(grouped(g.insert(1, @[Texel(t)]))) == "\nChris\t")
+      check(get_text(grouped(g.insert(2, @[Texel(t)]))) == "\n\tChris")
+      expect(IndexError):
+        discard g.insert(-1, @[Texel(t)])
 
-        check(get_text(grouped(t.insert(0, @[Texel(g)]))) == "\n\tChris")
-        check(get_text(grouped(t.insert(1, @[Texel(g)]))) == "C\n\thris")
-        check(get_text(grouped(t.insert(2, @[Texel(g)]))) == "Ch\n\tris")
-        check(get_text(grouped(t.insert(3, @[Texel(g)]))) == "Chr\n\tis")
-        check(get_text(grouped(t.insert(4, @[Texel(g)]))) == "Chri\n\ts")
-        check(get_text(grouped(t.insert(5, @[Texel(g)]))) == "Chris\n\t")
-        expect(IndexError):
-          discard t.insert(6, @[Texel(g)])
-          
-        expect(IndexError):
-          discard t.insert(-1, @[Texel(g)])
+      expect(IndexError):
+        discard g.insert(3, @[Texel(t)])
 
-        var h = grouped(t.insert(2, @[Texel(g)]))
-        #echo h
-        let hh = h.insert(3, @[Texel(g)])
-        assert len(hh) == 1
-        assert depth(hh[0]) == 1
+      check(get_text(grouped(t.insert(0, @[Texel(g)]))) == "\n\tChris")
+      check(get_text(grouped(t.insert(1, @[Texel(g)]))) == "C\n\thris")
+      check(get_text(grouped(t.insert(2, @[Texel(g)]))) == "Ch\n\tris")
+      check(get_text(grouped(t.insert(3, @[Texel(g)]))) == "Chr\n\tis")
+      check(get_text(grouped(t.insert(4, @[Texel(g)]))) == "Chri\n\ts")
+      check(get_text(grouped(t.insert(5, @[Texel(g)]))) == "Chris\n\t")
+      expect(IndexError):
+        discard t.insert(6, @[Texel(g)])
 
-        # absichern, dass durch das Slicing eine Kopie erzeugt wird:
-        var r = @[0, 1, 2, 3, 4]
-        var s = r[2..^1]
-        s[1] = -1
-        assert s == @[2, -1, 4]
-        assert r == @[0, 1, 2, 3, 4]
+      expect(IndexError):
+        discard t.insert(-1, @[Texel(g)])
+
+      var h = grouped(t.insert(2, @[Texel(g)]))
+      #echo h
+      let hh = h.insert(3, @[Texel(g)])
+      assert len(hh) == 1
+      assert depth(hh[0]) == 1
+
+      # absichern, dass durch das Slicing eine Kopie erzeugt wird:
+      var r = @[0, 1, 2, 3, 4]
+      var s = r[2..^1]
+      s[1] = -1
+      assert s == @[2, -1, 4]
+      assert r == @[0, 1, 2, 3, 4]
 
