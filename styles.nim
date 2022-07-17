a6bcad301e43bc5df01814b9414f19be6bcaef5c
ecker <textmodelview@gmail.com>
2022-07-17 18:24:06 +0200

    Added style pool.

--- styles.nim
+++ styles.nim
@@ -1,5 +1,7 @@
 import options
 import strutils
+import hashes
+import tables
 
 
   
@@ -7,12 +9,26 @@ type
   Style* = ref object
     fontsize*: Option[int]
     textcolor*: Option[string]
-          
+
+
+proc hash(x: Style): Hash = 
+  var empty = true
+  for key, value in fieldPairs(x[]):
+    if value.isSome:
+      if empty:
+        result = value.get().hash
+        empty = false
+      else:
+        result = result !& value.get().hash
+      result = !$result
+
+      
 proc `or`[T](self: Option[T], other: Option[T]): Option[T] =
   if self.isNone:
     return other
   return self
 
+  
 proc `or`*(x: Style, y: Style): Style =
     result = Style()
     # copy
@@ -22,16 +38,38 @@ proc `or`*(x: Style, y: Style): Style =
     for name, v, r in fieldPairs(y[], result[]):
       r = r or v
     return result
+
     
 proc `$`*(x: Style): string =
   var l = newSeq[string](0)
   for name, v in fieldPairs(x[]):
     if not v.isNone:
       l.add(name & ": " & $v)
-  return "Style("&l.join(", ")&")" 
+  return "Style("&l.join(", ")&")"
 
-let EMPTY_STYLE* = Style()
-let DEFAULT_STYLE* = Style(fontsize: option(12), textcolor: option("black"))
+
+# Hash tables compare objects using `==` which by default compares the
+# pointer addresses. Since we want a comparison by values, we need to
+# redefine the equality operator. 
+proc `==`*(x: Style, y: Style): bool = 
+  for c, d in fields(x[], y[]):
+    if not (c==d): return false
+  return true
+  
+
+var
+  pool = initTable[Style, Style]()
+
+  
+proc pooled*(style: Style): Style =
+  if style in pool:
+    return pool[style]
+  pool[style] = style
+  return style
+
+  
+let EMPTY_STYLE* = pooled(Style())
+let DEFAULT_STYLE* = pooled(Style(fontsize: option(12), textcolor: option("black")))
 
 
 when isMainModule:
@@ -57,6 +95,20 @@ when isMainModule:
       let b = Style(textcolor: option("red"))
       let c = Style(textcolor: option("black"))
       check((b or c)[] == Style(textcolor: option("red"))[])
+
+    test "style equality":
+      let b = Style(textcolor: option("red"))
+      let c = Style(textcolor: option("red"))
+      let d = Style(textcolor: option("black"))
+      check(b==c)
+      check(b!=d)
+
+    test "pooling styles":
+      let b = pooled(Style(textcolor: option("red")))
+      let c = pooled(Style(textcolor: option("red")))
+      let d = pooled(Style(textcolor: option("black")))        
+      check(b[].unsafeAddr == c[].unsafeAddr)      
+      check(b[].unsafeAddr!=d[].unsafeAddr)
 
 
      
